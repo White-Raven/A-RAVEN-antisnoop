ea18c0b34208677918b2dd14438bfa096ac230c0
White-Raven <anascroll@hotmail.fr>
2021-03-26 16:37:02 +0100

    Create pages.php

---
+++ pages.php
@@ -0,0 +1,4 @@
+<!-- to declare in any page which is to be requested directly under normal circumstances, for exx index.php, basically anything that your users are to click a link to -->
+<?php
+define('_DEFVAR', 1); //you can obviously call it whatever
+?>
