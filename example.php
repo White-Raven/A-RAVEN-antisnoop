47585fe15e6fac4cc888582b3495bd7eadcca88a
White-Raven <anascroll@hotmail.fr>
2021-03-26 16:38:46 +0100

    Update and rename example.php to includes.php

--- example.php
+++
@@ -1,2 +0,0 @@
-<!-- To add at the top of any page you don't want accessed directly, for exx header.php, footer.php, ect -->
-<?php require_once( $_SERVER['DOCUMENT_ROOT'] ."/inc/check.php"); ?>
---
+++ includes.php
@@ -0,0 +1,5 @@
+<?php 
+/* To add at the top of any page you don't want accessed directly, for exx header.php, 
+footer.php, basically anything that is to be requested only through includes, requires, ect */
+require_once( $_SERVER['DOCUMENT_ROOT'] ."/inc/check.php"); 
+?>
