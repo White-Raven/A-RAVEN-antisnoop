031aab669d820a218d2db2dc610e0d1f2d27a030
White-Raven <anascroll@hotmail.fr>
2021-03-26 16:39:36 +0100

    Update pages.php

--- pages.php
+++ pages.php
@@ -1,4 +1,5 @@
-<!-- to declare in any page which is to be requested directly under normal circumstances, for exx index.php, basically anything that your users are to click a link to -->
-<?php
+<?php 
+/* to declare in any page which is to be requested directly under normal circumstances, 
+for exx index.php, basically anything that your users are to click a link to */
 define('_DEFVAR', 1); //you can obviously call it whatever
 ?>
